5756a53e4e209c1dbc3a999a36fe7dacd0a1b244
dialop <dianalop1998@gmail.com>
2024-03-10 01:31:52 -0500

    Remove comment config file

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}", // this will include all your JavaScript and TypeScript files in src folder
+    "./src/**/*.{js,jsx,ts,tsx}", 
     "./public/index.html",
   ],
   theme: {

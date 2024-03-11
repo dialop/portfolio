2e5ef6a60587b289ac85416b4022102896d9edd5
dialop <dianalop1998@gmail.com>
2024-03-10 20:50:06 -0400

    Add About div in main content component

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,21 +1,23 @@
-
 const Header = () => {
     return (
       <header className="py-8 bg-transparent">
         <div className="container mx-auto flex justify-between items-center px-4 sm:px-6 lg:px-8">
-            <div>
-          <h1 className="text-3xl font-bold text-white">DIANA LOPEZ</h1>
-          <p className="text-md font-light text-gray-400">Full Stack Developer</p>
+          <div>
+            <h1 className="text-3xl font-bold text-white">DIANA LOPEZ</h1>
+            <p className="text-md font-light text-gray-400">Full Stack Developer</p>
           </div>
           <nav>
-            <ul className="flex space-x-8">
-              <li><a href="/work" className="text-lg text-white hover:text-gray-300">Work</a></li>
-              <li><a href="/about" className="text-lg text-white hover:text-gray-300">About</a></li>
-            </ul>
+            <div className="flex">
+              <a href="/work" className="text-lg text-white hover:text-gray-300 mr-16">Work</a>
+            </div>
+            <div className="flex">
+              <a href="/about" className="text-lg text-white hover:text-gray-300 mr-16">About</a>
+            </div>
           </nav>
         </div>
       </header>
     );
-};
-
-export default Header;
+  };
+  
+  export default Header;
+  

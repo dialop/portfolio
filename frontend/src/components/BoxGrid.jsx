354a27ee3759df245d4d1751488e5d95f2916541
dialop <dianalop1998@gmail.com>
2024-03-11 21:48:21 -0400

    Add project photo and add images in public folder

--- frontend/src/components/BoxGrid.jsx
+++ frontend/src/components/BoxGrid.jsx
@@ -6,10 +6,16 @@ const BoxGrid = () => {
     {
       title: 'InstaStaff',
       content: 'Dashboard Design',
+      imageURL: '/images/photolabs_image.jpeg',
+      alt: 'Instastaff Dashboard'
+
     },
     {
       title: 'Photolabs',
       content: 'Dashboard Design',
+      imageURL: '/images/instastaff_homepage.jpeg',
+      alt: 'Photolabs Dashboard'
+
     },
   ];
 
@@ -17,7 +23,13 @@ const BoxGrid = () => {
     <div className="container mx-auto px-4">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {projectData.map((project, index) => (
-          <ProjectBox key={index} title={project.title} content={project.content} />
+          <ProjectBox 
+          key={index} 
+          title={project.title} 
+          content={project.content}
+          imageURL={project.imageURL}
+          alt={project.alt}
+          />
         ))}
       </div>
     </div>
--- frontend/src/components/ProjectBox.jsx
+++ frontend/src/components/ProjectBox.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 
-const ProjectBox = ({ title, content }) => {
+const ProjectBox = ({ title, content, imageURL, alt }) => {
   return (
-    <div className="bg-white shadow-lg rounded-lg p-6 m-6">
+    <div className="bg-white shadow-lg rounded-lg p-6 m-6"> 
+    {imageURL && <img imageURL={imageURL} alt={alt || tittle} className="rounded-lg mb-4" />}
       <h2 className="font-bold text-xl mb-4">{title}</h2>
       <p>{content}</p>
     </div>

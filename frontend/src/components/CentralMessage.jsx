d99964a6bede4515c6b5ec7e8516ef35aaec801e
dialop <dianalop1998@gmail.com>
2024-03-12 22:52:08 -0400

    Add more central message content

--- frontend/src/components/CentralMessage.jsx
+++ frontend/src/components/CentralMessage.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 
 const CentralMessage = () => {
-    return (
-      <div className="text-center my-20 px-4 lg:pr-20">
-        <h1 className="text-6xl lg:text-8xl font-bold text-gray-800 leading-none">Hi, I'm Diana</h1>
-        <p className="text-3xl text-gray-500 mt-4"> </p>
-      </div>
-    );
-  };
-  
-  export default CentralMessage;
+  return (
+    // Apply padding right on large screens
+    <div className="text-center mt-20 lg:pr-32">
+      <h1 className="text-6xl font-bold text-gray-800 leading-tight">Hi, I'm Diana</h1>
+      <p className="text-3xl text-gray-500 mt-4">Full Stack Web Developer</p>
+    </div>
+  );
+};
+
+export default CentralMessage;

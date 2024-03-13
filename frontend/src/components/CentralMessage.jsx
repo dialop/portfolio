29bdbe96339d2e5749e7928d557f553e087113a5
dialop <dianalop1998@gmail.com>
2024-03-12 22:54:19 -0400

    Add central message style to go under sentance

--- frontend/src/components/CentralMessage.jsx
+++ frontend/src/components/CentralMessage.jsx
@@ -2,9 +2,8 @@ import React from "react";
 
 const CentralMessage = () => {
   return (
-    // Apply padding right on large screens
-    <div className="text-center mt-20 lg:pr-32">
-      <h1 className="text-6xl font-bold text-gray-800 leading-tight">Hi, I'm Diana</h1>
+<div className="container mx-auto items-center px-4 sm:px-6 lg:px-8" style={{ paddingTop: '2rem', paddingBottom: '6rem'}}>     
+ <h1 className="text-6xl font-bold text-gray-800 leading-tight">Hi, I'm Diana</h1>
       <p className="text-3xl text-gray-500 mt-4">Full Stack Web Developer</p>
     </div>
   );

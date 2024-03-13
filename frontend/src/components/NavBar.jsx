ed3b9fc6f4ee177079c3b98d7e80dc7b57c64555
dialop <dianalop1998@gmail.com>
2024-03-12 21:54:58 -0400

    Remove style attibution from heading and place in below div

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const NavBar = () => {
     return (
-        <header className="py-8 bg-transparent" style={{ paddingTop: '2rem', paddingBottom: '6rem'}}  > 
-            <div className="container mx-auto flex justify-between items-center px-4 sm:px-6 lg:px-8">
+        <header className="py-8 bg-transparent"  > 
+            <div className="container mx-auto flex justify-between items-center px-4 sm:px-6 lg:px-8" style={{ paddingTop: '2rem', paddingBottom: '6rem'}}>
                 <div> 
                     <h1 className="text-3xl font-bold text-black">DIANA LOPEZ </h1>
                     <p className="text-md font-light text-gray-400">Full Stack Developer</p>

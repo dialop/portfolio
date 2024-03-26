644665b6edf1b911f463e073b46fa9be6c28e6a8
dialop <dianalop1998@gmail.com>
2024-03-25 22:59:21 -0400

    Refractor About component

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -1,11 +1,9 @@
-import React from "react";
-
 const About = () => {
   return (
-<div className="container mx-auto items-center px-4 sm:px-6 lg:px-8" style={{ paddingTop: '2rem', paddingBottom: '6rem'}}>     
- <h1 className="text-7xl font-bold text-text-#CBD5E0 leading-tight text-center" style={{ paddingTop: '2rem', paddingBottom: '4rem'}}>Hi, I'm Diana</h1>
-      <p className="text-2xl text-text-#CBD5E0 mt-4 text-center"> Junior Full Stack Developer with a healthcare background</p>
-       <p className="text-2xl text-text-#CBD5E0 mt-4 text-center">dedicated to creating impactful and user-friendly tech solutions.</p>
+    <div className="container mx-auto items-center px-4 sm:px-6 lg:px-8 py-8">     
+      <h1 className="text-7xl font-bold leading-tight text-center text-text-#CBD5E0 pb-16">Hi, I'm Diana</h1>
+      <p className="text-2xl text-text-#CBD5E0 mt-4 text-center">Junior Full Stack Developer with a healthcare background</p>
+      <p className="text-2xl text-text-#CBD5E0 text-center">dedicated to creating impactful and user-friendly tech solutions.</p>
     </div>
   );
 };

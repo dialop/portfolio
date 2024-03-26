6efb5e9b8087da3042a58beb6f8eb62d89522200
dialop <dianalop1998@gmail.com>
2024-03-25 22:54:52 -0400

    Add p tag for about section

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -3,9 +3,9 @@ import React from "react";
 const About = () => {
   return (
 <div className="container mx-auto items-center px-4 sm:px-6 lg:px-8" style={{ paddingTop: '2rem', paddingBottom: '6rem'}}>     
- <h1 className="text-7xl font-bold text-text-#CBD5E0 leading-tight text-center">Hi, I'm Diana</h1>
-      <p className="text-2xl text-text-#CBD5E0 mt-4 text-center"> Junior Full Stack Developer
-       with a healthcare background dedicated to creating impactful and user-friendly tech solutions. </p>
+ <h1 className="text-7xl font-bold text-text-#CBD5E0 leading-tight text-center" style={{ paddingTop: '2rem', paddingBottom: '4rem'}}>Hi, I'm Diana</h1>
+      <p className="text-2xl text-text-#CBD5E0 mt-4 text-center"> Junior Full Stack Developer with a healthcare background</p>
+       <p className="text-2xl text-text-#CBD5E0 mt-4 text-center">dedicated to creating impactful and user-friendly tech solutions.</p>
     </div>
   );
 };

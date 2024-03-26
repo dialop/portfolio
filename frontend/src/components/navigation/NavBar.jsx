46c70e9bacd5afd07c03045175a318dc7529d40c
dialop <dianalop1998@gmail.com>
2024-03-25 23:07:01 -0400

    Add reload onclick cursor point on navbar name

--- frontend/src/components/navigation/NavBar.jsx
+++ frontend/src/components/navigation/NavBar.jsx
@@ -3,13 +3,18 @@ import ChipTabs from './ChipTabs';
 import DarkModeToggle from './DarkModeToggle';
 
 const NavBar = ({ darkMode, setDarkMode }) => {
+
+    const refreshPage = () => {
+        window.location.reload();
+    };
+
     return (
-        <header className={`py-8 ${darkMode ? 'text-#CBD5E0' : 'text-black'}`}>
+        <header className={`py-8 ${darkMode ? 'text-customColorName' : 'text-black'}`}>
             <div className="container mx-auto flex justify-between items-center px-4 sm:px-6 lg:px-8 space-x-4">
-                <div>
+                <a onClick={refreshPage} className="cursor-pointer">
                     <h1 className="text-3xl font-bold">DIANA LOPEZ</h1>
                     <p className="text-md font-light text-gray-400">Full Stack Developer</p>
-                </div>
+                </a>
                 <div className="flex items-center space-x-4">
                     <ChipTabs />
                     <DarkModeToggle darkMode={darkMode} setDarkMode={setDarkMode} />

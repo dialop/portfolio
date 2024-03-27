153d0354b9e5262bf5accf4b48d4aac136948eda
dialop <dianalop1998@gmail.com>
2024-03-26 21:36:33 -0400

    Add darkmode toggle style

--- frontend/src/components/navigation/DarkModeToggle.jsx
+++ frontend/src/components/navigation/DarkModeToggle.jsx
@@ -25,7 +25,7 @@ const DarkModeToggle = ({ darkMode, setDarkMode }) => {
 
         {/* Sun Icon */}
         <div className={`icon icon--sun ${darkMode ? 'scale-100 opacity-100 rotate-360' : 'scale-0 opacity-0'} transition-transform duration-500 ease-in-out delay-200`}>
-          <svg className="w-19.5 h-20 text-yellow-500 flex items-center justify-center" fill="currentColor" viewBox="0 0 26 65" xmlns="http://www.w3.org/2000/svg">
+          <svg className="w-19 h-20 text-yellow-500 flex justify-center" fill="currentColor" viewBox="0 8 26 65" xmlns="http://www.w3.org/2000/svg">
             <path d="M12 2.25a.75.75 0 01.75.75v2.25a.75.75 0 01-1.5 0V3a.75.75 0 01.75-.75zM7.5 12a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM18.894 6.166a.75.75 0 00-1.06-1.06l-1.591 1.59a.75.75 0 101.06 1.061l1.591-1.59zM21.75 12a.75.75 0 01-.75.75h-2.25a.75.75 0 010-1.5H21a.75.75 0 01.75.75zM17.834 18.894a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 10-1.061 1.06l1.59 1.591zM12 18a.75.75 0 01.75.75V21a.75.75 0 01-1.5 0v-2.25A.75.75 0 0112 18zM7.758 17.303a.75.75 0 00-1.061-1.06l-1.591 1.59a.75.75 0 001.06 1.061l1.591-1.59zM6 12a.75.75 0 01-.75.75H3a.75.75 0 010-1.5h2.25A.75.75 0 016 12zM6.697 7.757a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 00-1.061 1.06l1.59 1.591z"></path>
           </svg>
         </div>

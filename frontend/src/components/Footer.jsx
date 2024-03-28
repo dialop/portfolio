045b9bc0e01c211a604431be9df74d16fd2fb5fb
dialop <dianalop1998@gmail.com>
2024-03-27 22:54:11 -0400

    Remove footer line

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -5,18 +5,15 @@ function Footer() {
 
   return (
     <div className="container mx-auto">
-      <div className="pt-20 sm:pt-30 pb-8 mt-20 border-t-2 border-primary-light dark:border-secondary-dark">
         <div className="text-gray-400 text-sm text-center">
           <a
             href={githubLink}
-            target="_blank"
             rel="noopener noreferrer"
             className="text-grey dark:text-grey hover:text-indigo-500 dark:hover:text-indigo-400"
           >
             Created & Designed by Diana © 2024
           </a>
         </div>
-      </div>
     </div>
   );
 }
